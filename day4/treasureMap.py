8f4dca21dd416e65fbd023030eee5159222c5abc
greenMakaroni <mrtn.dawid@gmail.com>
2022-07-11 23:42:09 +0100

    day 4 in progress

--- day4/treasureMap.py
+++ day4/treasureMap.py
@@ -9,13 +9,16 @@ row2 = ["🕳","🕳","🕳"]
 row3 = ["🕳","🕳","🕳"]
 
 map = [row1, row2, row3]
-print(f"{row1}\n{row2}\n{row3}")
+
+print("   1   2   3")
+print(f"1{row1}\n2{row2}\n3{row3}")
 position = input("Where do you want to put the treasure? ")
 # Don't change the code above
 
-firstDigit = int(position[0])
-secondDigit = int(position[1])
+firstDigit = int(position[0]) -1
+secondDigit = int(position[1]) -1
 
 map[firstDigit][secondDigit] = "💎"
 
-print(f"{row1}\n{row2}\n{row3}")
+print("   1   2   3")
+print(f"1{row1}\n2{row2}\n3{row3}")
