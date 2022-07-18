841b2841ab9147a4173713fbc2a48b73ecdb741a
greenMakaroni <mrtn.dawid@gmail.com>
2022-07-18 07:51:40 +0100

    day6 in progress

--- day6/functions.py
+++ day6/functions.py
@@ -2,3 +2,29 @@ print("Hello")
 
 num_char = len("Hello")
 print(num_char)
+
+
+# define functions
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# call functions
+my_function()
+
+# indentation is syntax
+sky = "clear"
+
+def my_function2():
+    if sky == "clear":
+        print("blue")
+    elif sky == "cloudy":
+        print("grey")
+    print("hello")
+print("World")
+my_function2()
+
+def my_function3():
+  print("lel")
+
+my_function3()
