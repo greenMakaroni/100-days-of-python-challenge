8ba43afe4edbcc9908cce660595147f206265406
greenMakaroni <mrtn.dawid@gmail.com>
2022-07-25 07:24:21 +0100

    Day 13 complete

--- day 013/debugging.py
+++ day 013/debugging.py
@@ -1,31 +1,54 @@
 ############DEBUGGING#####################
 
-# # Describe Problem
+
+# # Describe Problem ****** uncomment and debug
 # def my_function():
 #   for i in range(1, 20):
 #     if i == 20:
 #       print("You got it")
 # my_function()
 
-# # Reproduce the Bug
+def my_function():
+  for i in range(1, 20 + 1):
+    if i == 20:
+      print("You got it")
+my_function()
+
+# # Reproduce the Bug ****** uncomment and debug
 # from random import randint
 # dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
 # dice_num = randint(1, 6)
 # print(dice_imgs[dice_num])
 
-# # Play Computer
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
+
+# # Play Computer ****** uncomment and debug
 # year = int(input("What's your year of birth?"))
 # if year > 1980 and year < 1994:
 #   print("You are a millenial.")
 # elif year > 1994:
 #   print("You are a Gen Z.")
 
-# # Fix the Errors
+year = int(input("What's your year of birth?"))
+if year > 1980 and year <= 1994:
+  print("You are a millenial.")
+elif year > 1994:
+  print("You are a Gen Z.")
+
+# # Fix the Errors ****** uncomment and debug
 # age = input("How old are you?")
 # if age > 18:
 # print("You can drive at age {age}.")
 
-# #Print is Your Friend
+age = int(input("How old are you?"))
+if age > 18:
+    print(f"You can drive at age {age}.")
+
+
+# #Print is Your Friend ****** uncomment and debug
 # pages = 0
 # word_per_page = 0
 # pages = int(input("Number of pages: "))
@@ -33,9 +56,23 @@
 # total_words = pages * word_per_page
 # print(total_words)
 
-# #Use a Debugger
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
+
+# #Use a Debugger ****** uncomment and debug
 # def mutate(a_list):
 #   b_list = []
 #   for item in a_list:
 #     new_item = item * 2
 #   b_list.append(new_item)
+
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+mutate([2,4,6])
