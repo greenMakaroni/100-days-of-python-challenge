c5ecd58827e8d934706e3559f6e1d2fcee87b1a8
greenMakaroni <mrtn.dawid@gmail.com>
2022-07-28 11:48:18 +0100

    day 16 started

---
+++ day 016/oop.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+# init object
+timmy = Turtle()
+my_screen = Screen()
+print(timmy)
+
+# object attributes
+timmy.shape("turtle")
+timmy.color("DarkBlue")
+
+#object methods
+timmy.forward(100)
+print(my_screen.canvheight)
+my_screen.exitonclick()
