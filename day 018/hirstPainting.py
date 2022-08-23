16abaffe8796440cb619ea40ac2e34d96f6d2abd
greenMakaroni <mrtn.dawid@gmail.com>
2022-08-23 07:34:39 +0100

    Day 18 complete

--- day 018/hirstPainting.py
+++ day 018/hirstPainting.py
@@ -3,29 +3,40 @@ import random
 
 t.colormode(255)
 timmy = t.Turtle()
-timmy.width(7)
 timmy.speed("fastest")
 
 # random rgb generator
 def randomColor():
     return (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
 
+x = -300
+y = -200
+
 timmy.penup()
 timmy.setpos(-300, -200)
 
 def drawPainting():
+    global x, y
+
     def drawColumn():
-        for i in range(1, 10):            
+        for _ in range(1, 11):            
             timmy.color(randomColor())
             timmy.begin_fill()
             timmy.circle(10)
             timmy.end_fill()
+            timmy.penup()
+            timmy.forward(40)
+            timmy.pendown()
+    
+    for _ in range(1, 11):
+        y += 40
+        timmy.penup()
+        timmy.setpos(x, y)
+        drawColumn()
+    
+drawPainting()
 
 
-timmy.color(randomColor())
-timmy.begin_fill()
-timmy.circle(10)
-timmy.end_fill()
 
 screen = t.Screen()
 screen.exitonclick()
