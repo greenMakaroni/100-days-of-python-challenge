62218777eefb500393d0a885c2f84fa5166f2dfa
greenMakaroni <mrtn.dawid@gmail.com>
2022-11-12 13:05:36 +0000

    Day 28 'Pomodoro app' complete

--- day 028/main.py
+++ day 028/main.py
@@ -9,11 +9,22 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-REPS = 1
+REPS = 0
+CHECKMARKS = ""
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
+def reset():
+    REPS = 0
+    CHECKMARKS = ""
+    window.after_cancel(timer)
+    title_label.config(text="Timer",bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50))
+    canvas.itemconfig(timer_text, text="25:00")
+    check_mark.config(text=CHECKMARKS)
+
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+
 # Pomodoro technique
 # 25 min work
 # 5 min break
@@ -26,21 +37,24 @@ REPS = 1
 
 def start_timer():
     global REPS
+    REPS += 1
 
-    if REPS == 8:
-        count_down(20 * 60)
-        REPS = 1
-    elif REPS < 8 and REPS % 2 != 0:
-        count_down(25 * 60)
-        REPS += 1
+    if REPS % 8 == 0:
+        count_down(LONG_BREAK_MIN * 60)
+        title_label.config(text="Break", fg=RED)
+    elif REPS % 2 == 0:
+        count_down(SHORT_BREAK_MIN * 60)
+        title_label.config(text="Break", fg=PINK)
     else:
-        count_down(5 * 60)
-        REPS += 1
-
+        count_down(WORK_MIN * 60)
+        title_label.config(text="Work", fg=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
 def count_down(count):
     global window
+    global CHECKMARKS
+    global timer
     minutes = math.floor(count / 60)
     seconds = count % 60
 
@@ -54,12 +68,15 @@ def count_down(count):
     # way to mutate element in a canvas
     canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
-        print(count)
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        CHECKMARKS = ""
+        for _ in range(math.floor(REPS / 2)):
+            CHECKMARKS += "✔"
+        check_mark.config(text=CHECKMARKS)
 
 # ---------------------------- UI SETUP ------------------------------- #
-
-
 
 # Window setup
 window = Tk()
@@ -74,11 +91,11 @@ title_label.grid(column=1, row=0)
 # Buttons
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset)
 reset_button.grid(column=2, row=2)
 
 # Checkmarks
-check_mark = Label(text="✔", highlightthickness=0, bg=YELLOW, fg=GREEN)
+check_mark = Label(highlightthickness=0, bg=YELLOW, fg=GREEN)
 check_mark.grid(column=1, row=3)
 
 # canvas widgets
