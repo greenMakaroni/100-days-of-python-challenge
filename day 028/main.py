42f8ff9cae7c350925badb1110d45bc79a2dfe96
greenMakaroni <mrtn.dawid@gmail.com>
2022-11-12 11:35:45 +0000

    Day 28 UI complete

--- day 028/main.py
+++ day 028/main.py
@@ -20,14 +20,28 @@ LONG_BREAK_MIN = 20
 # Window setup
 window = Tk()
 window.title("Python Pomodoro")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# Label element
+title_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+# Buttons
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+# Checkmarks
+check_mark = Label(text="✔", highlightthickness=0, bg=YELLOW, fg=GREEN)
+check_mark.grid(column=1, row=3)
 
 # canvas widgets
 tomato_image = PhotoImage(file="tomato.png")
-canvas = Canvas(width=200, height=224)
-canvas.create_image(102, 112, image=tomato_image)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas.create_image(100, 112, image=tomato_image)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
 
 
