aa9039632dd9f4eda17332faf28011fbc1b9ada1
greenMakaroni <mrtn.dawid@gmail.com>
2022-11-19 21:26:56 +0000

    saving data to a file

--- day 029/main.py
+++ day 029/main.py
@@ -3,6 +3,18 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    website = website_entry.get()
+    username = email_entry.get()
+    password = password_entry.get()
+
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {username} | {password}\n")
+
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+
 # ---------------------------- UI SETUP ------------------------------- #
 print("he")
 
@@ -27,8 +39,10 @@ password_label.grid(column=0, row=3)
 
 # Inputs
 website_entry = Entry(width=35)
+website_entry.focus()
 website_entry.grid(column=1, row=1, columnspan=2)
 email_entry = Entry(width=35)
+email_entry.insert(0, "somerandomemail@gmail.com")
 email_entry.grid(column=1,row=2, columnspan=2)
 password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3)
@@ -36,7 +50,7 @@ password_entry.grid(column=1, row=3)
 # Buttons
 generate_password_button = Button(text="Generate password")
 generate_password_button.grid(column=2, row=3)
-add_password_button = Button(text="Add Password", width=36)
+add_password_button = Button(text="Add Password", width=36, command=save)
 add_password_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
