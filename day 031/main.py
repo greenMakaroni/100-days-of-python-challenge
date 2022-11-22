edd837eca9bebdb20b89df24cda73546041bcdba
greenMakaroni <mrtn.dawid@gmail.com>
2022-11-22 12:31:00 +0000

    Day 31 complete

--- day 031/main.py
+++ day 031/main.py
@@ -2,11 +2,42 @@
 BACKGROUND_COLOR = "#B1DDC6"
 
 from tkinter import *
+import pandas
+import random
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pandas.read_csv("data/french_words.csv")
+
+to_learn = data.to_dict(orient="records")
+current_card = {}
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_bg, image=card_front_image)
+    flip_timer = window.after(3000, func=flip_card)
+def flip_card():
+    global current_card
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_bg, image=card_back_image)
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv")
+    next_card()
 
 # window setup
 window = Tk()
 window.title("Flash cards")
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
 
 # canvas setup
 canvas = Canvas(width=800, height=526)
@@ -18,27 +49,20 @@ card_back_image = PhotoImage(file="images/card_back.png")
 cross_image = PhotoImage(file="images/wrong.png")
 tick_image = PhotoImage(file="images/right.png")
 
-canvas.create_image(400, 263, image=card_front_image)
+card_bg = canvas.create_image(400, 263, image=card_front_image)
 canvas.grid(row=0, column=0, columnspan=2)
 
 # buttons setup
-red_button = Button(image=cross_image)
-green_button = Button(image=tick_image)
+red_button = Button(image=cross_image, command=next_card)
+green_button = Button(image=tick_image, command=is_known)
 green_button.grid(row=1, column=0)
 red_button.grid(row=1, column=1)
 
 # text setup
-canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Arial", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 
-
-
-
-
-
-
-
-
+next_card()
 
 # keep main window open in a loop
 window.mainloop()
